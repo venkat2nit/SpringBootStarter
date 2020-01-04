0a2d66b4404a9127bee84ebc71fad2215d83b2fb
venkat guntaka <venkat2nit@gmail.com>
2020-01-04 13:27:46 -0500

    My first Commit

---
+++ src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.in28minutes.springboot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model){
+		return "login";
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String showWelcomePage(ModelMap model, @RequestParam String name){
+		model.put("name", name);
+		return "welcome";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+<title>First Web Application</title>
+</head>
+
+<body>
+	<form method="post">
+		Name : <input type="text" name="name" />
+		Password : <input type="password" name="password" /> 
+		<input type="submit" />
+	</form>
+</body>
+
+</html>
